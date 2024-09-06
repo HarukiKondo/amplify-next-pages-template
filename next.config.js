87d4ef5ab98bb366b8761b878e16b697959459bb
HarukiKondo <haruki-kondo@secom.co.jp>
2024-09-06 17:39:59 +0900

    PWAに対応させました。

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+});
+
+module.exports = nextConfig;
